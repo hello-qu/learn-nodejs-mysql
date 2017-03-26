800743f4b8909c18c71a4af2b8c3b20823f4abc5
qujun <qujun_1992@163.com>
2017-03-26 17:05:00 +0800

    同步异步捕获错误机制:

---
+++ n9_expection.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var url = require('url');
+var route = require('./module/router')
+http.createServer(function (resquest, response) {
+    if (resquest.url !== '/favicon.ico') {
+        var pathname = url.parse(resquest.url).pathname;
+        pathname = pathname.replace(/\//, '');
+        try {
+            route[pathname](response);
+        } catch (error) {
+            console.log('--'+error+'--');
+             response.writeHead(200, { 'Content-Type': 'text/html;charset=uft-8' });
+             response.write(error.toString());
+             response.end();
+        }
+        console.log('主程序执行完毕')
+    }
+}).listen(8899);
+
+console.log('sever start at http://localhost:8899');
